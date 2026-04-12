615a5c09937030f80c23bba0b69a42d4410c20d0
Emiwiana <a51635@alunos.isel.pt>
2026-04-12 22:05:17 +0100

    Add vector weather icons and update UI logic
    Add a set of vector drawable weather icons and update MainActivity/WeatherData to use them. Changes: add many drawable XML assets for weather conditions; clean up WMO_WeatherCode enum image names (remove extra spaces) and provide mapping; fix API request string formatting; assemble UI text into local variables and set TextViews; improve resource lookup (getIdentifier, check resID != 0) and set image drawable; start the weather fetch thread (.start()); change default day flag to false; add Resources import and suppress DiscouragedApi lint. Also include minor layout updates (activity_main and layout-land). These changes make weather icons map correctly to weather codes and stabilize UI updates.

--- TP2/android/app/src/main/java/dam_A51635/coolweatherapp/MainActivity.kt
+++ TP2/android/app/src/main/java/dam_A51635/coolweatherapp/MainActivity.kt
@@ -2,6 +2,8 @@ package dam_A51635.coolweatherapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,24 +12,36 @@ import com.google.gson.Gson
 import java.io.InputStreamReader
 import java.net.URL
 
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        fetchWeatherData(38.72f, 9.15f)
+        val updateButton : Button = findViewById(R.id.btnUpdate)
+        val latitudeText : EditText = findViewById(R.id.etLatitude)
+        val longitudeText : EditText = findViewById(R.id.etLongitude)
+
+        updateButton.setOnClickListener {
+            var latitude: Float = latitudeText.text.toString().toFloat()
+            var longitude = longitudeText.text.toString().toFloat()
+
+            if(latitude in -180f..<180f && longitude in -180f..<180f)
+                fetchWeatherData(latitude, longitude).start()
+        }
     }
 
-    var day = true
 
     private fun WeatherAPI_Call ( lat: Float , long : Float ) : WeatherData {
         val reqString = buildString {
             append("https://api.open-meteo.com/v1/forecast?")
             append("latitude=${lat}&longitude=${long}&")
-            append("current_weather=true &")
-            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m ")
+            append("current_weather=true&")
+            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m")
         }
+
         val url = URL(reqString);
         url.openStream().use {
             val request = Gson().fromJson (InputStreamReader(it, "UTF-8"), WeatherData :: class.java )
@@ -38,14 +52,15 @@ class MainActivity : AppCompatActivity() {
     private fun fetchWeatherData (lat: Float , long : Float ) : Thread {
         return Thread {
             val weather = WeatherAPI_Call (lat , long )
-            updateUI(weather)
+            updateUI(weather, lat, long)
         }
     }
 
-    @SuppressLint("SetTextI18n", "UseCompatLoadingForDrawables")
-    private fun updateUI (request : WeatherData ) {
+    @SuppressLint("SetTextI18n", "UseCompatLoadingForDrawables", "DiscouragedApi")
+    private fun updateUI (request : WeatherData, lat: Float, long: Float) {
         runOnUiThread {
-            //val weatherImage : ImageView = findViewById(R.id.weatherImage )
+            val weatherImage : ImageView = findViewById(R.id.weatherImage )
+
 
             val pressure : TextView = findViewById(R.id.pressureValue)
             val windSpeed : TextView = findViewById(R.id.windSpeedValue)
@@ -53,30 +68,39 @@ class MainActivity : AppCompatActivity() {
             val temperature : TextView = findViewById(R.id.temperatureValue)
             val time : TextView = findViewById(R.id.timeValue)
 
-            pressure.text = request.hourly.pressure_msl[12].toString()+ " hPa"
-            windSpeed.text = request.current_weather.windspeed.toString()+ " km/H"
-            windDirection.text = request.current_weather.winddirection.toString()
-            temperature.text = request.current_weather.temperature.toString()+ " ºC"
-            time.text = request.current_weather.time
+            val pressureText = request.hourly.pressure_msl[12].toString()+ " hPa"
+            val windSpeedText = request.current_weather.windspeed.toString()+ " km/H"
+            val windDirectionText = request.current_weather.winddirection.toString()
+            val temperatureText = request.current_weather.temperature.toString()+ " ºC"
+            var timeText = request.current_weather.time
 
-            /*
-            // TODO ...
-            val mapt = getWeatherCodeMap ();
+
+            pressure.text = pressureText
+            windSpeed.text = windSpeedText
+            windDirection.text = windDirectionText
+            temperature.text = temperatureText
+            time.text = timeText
+
+            val hour = (request.current_weather.time[11].toString() +  request.current_weather.time[12].toString()).toInt()
+            val day = hour in 8..<20
+
+            val mapt = getWeatherCodeMap();
             val wCode = mapt[request.current_weather.weathercode]
             val wImage = when (wCode) {
                 WMO_WeatherCode.CLEAR_SKY,
                 WMO_WeatherCode.MAINLY_CLEAR,
-                WMO_WeatherCode.PARTLY_CLOUDY ->if(day) wCode?. image +"day" else
-                    wCode?.image +"night"
+                WMO_WeatherCode.PARTLY_CLOUDY ->if(day) wCode.image +"day" else
+                    wCode.image +"night"
                 else -> wCode?.image
             }
-            val res = getResources ()
-            weatherImage.setImageResource(R.drawable.fog)
-            val resID = res.getIdentifier (wImage , " drawable ", getPackageName ());
-            val drawable = this.getDrawable (resID);
-            weatherImage.setImageDrawable (drawable);
-            // TODO ...
-            */
+
+            val res = getResources()
+            val resID = res.getIdentifier (wImage , "drawable", packageName);
+            if (resID != 0){
+                val drawable = this.getDrawable(resID);
+                weatherImage.setImageDrawable(drawable);
+            }
         }
     }
+
 }
